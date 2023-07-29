6af307b08623d2348241f8c5c967a4a048477738
alikiminza <ali.kiminza@student.moringaschool.com>
2023-07-29 21:49:05 +0300

    last commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to="/Actors">Actors</NavLink>
+    <NavLink to="/Directors">Directors</NavLink>
+    <NavLink to="/Movies">Movies</NavLink>
+  </div>);
 }
 
 export default NavBar;
